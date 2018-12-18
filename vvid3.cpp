15200d316d10ba466fc57ae0c4ee49990891f9b5
ascendanceq <ascendanceq@gmail.com>
2018-12-18 20:46:31 +0700

    finalVers

---
+++ vvid3.cpp
@@ -0,0 +1,33 @@
+﻿#include "stdafx.h"
+#include <stdio.h>
+#include <cctype>
+#include <stdlib.h>
+#include <conio.h>
+
+int main()
+{
+	int currI = 0;
+	int currP = 0;
+	int currR = 0;
+	int currV = 0;
+	char check = '\0';
+	printf("Enter Amperage: ");
+	while (scanf_s("%d%c", &currI, &check, 1) != 2 || check != '\n' || currI < 0)
+	{
+		printf("Error! Try Again!\n");
+		while (getchar() != '\n');
+	}
+	printf("Enter Resistance: ");
+	while (scanf_s("%d%c", &currR, &check, 1) != 2 || check != '\n' || currR < 0)
+	{
+		printf("Error! Try Again!\n");
+		while (getchar() != '\n');
+	}
+	float test;
+	currV = currI * currR;
+	printf("Voltage = %d\n", currV);
+	currP = (currV * currI) / (currR * currV);
+	currP = currV * currI;
+	printf("Power = %d\n", currP);
+	system("pause");
+	return 0;
